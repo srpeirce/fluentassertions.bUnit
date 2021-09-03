52f78f5f17ec8d685e4ad4985e67b1d0aaeca03b
Steve Peirce <=>
2021-09-03 17:50:23 +0100

    Backfilled missing unit tests (incl. small fixes). #2
    Also tweaked pipeline so nuget package not pushed on PR

--- FluentAssertions.BUnit/ElementAssertions.cs
+++ FluentAssertions.BUnit/ElementAssertions.cs
@@ -3,7 +3,7 @@ using Bunit;
 
 namespace FluentAssertions.BUnit
 {
-    public static class BUnitAssertions
+    public static class ElementAssertions
     {
         public static IElement ShouldHaveTag(this IElement element, string expected)
         {
@@ -13,7 +13,9 @@ namespace FluentAssertions.BUnit
 
         public static IElement ShouldHaveChildMarkup(this IElement element, string expected)
         {
-            element.FirstChild.MarkupMatches(expected);
+            var child = element.FirstChild;
+            child.Should().NotBeNull();    
+            child!.MarkupMatches(expected);
             return element;
         }
 
@@ -29,63 +31,53 @@ namespace FluentAssertions.BUnit
             return element;
         }
 
-        public static IElement ShouldHaveTarget(this IElement element, string expected)
-        {
-            element.Attributes["target"].Value.Should().Be(expected);
-            return element;
-        }
+        public static IElement ShouldHaveId(this IElement element, string expected) 
+            => element.ShouldHaveAttribute("id", expected);
+        
+        public static IElement ShouldHaveTarget(this IElement element, string expected) 
+            => element.ShouldHaveAttribute("target", expected);
 
         public static IElement ShouldHaveRel(this IElement element, string expected)
         {
-            element.Attributes["rel"].Value.Should().Be(expected);
+            var attribute = element.Attributes["rel"];
+            
+            attribute.Should().NotBeNull();
+            attribute!.Value.Split(" ").Should().Contain(expected);
+            
             return element;
         }
 
         public static IElement ShouldHaveAriaLabel(this IElement element, string expected)
-        {
-            element.Attributes["aria-label"].Value.Should().Be(expected);
-            return element;
-        }
+            => element.ShouldHaveAttribute("aria-label", expected);
 
         public static IElement ShouldHaveHref(this IElement element, string expected)
-        {
-            element.Attributes["href"].Value.Should().Be(expected);
-            return element;
-        }
+            => element.ShouldHaveAttribute("href", expected);
 
         public static IElement ShouldHaveSrc(this IElement element, string expected)
-        {
-            element.Attributes["src"].Value.Should().Be(expected);
-            return element;
-        }
+            => element.ShouldHaveAttribute("src", expected);
 
         public static IElement ShouldHaveAlt(this IElement element, string expected)
-        {
-            element.Attributes["alt"].Value.Should().Be(expected);
-            return element;
-        }
+            => element.ShouldHaveAttribute("alt", expected);
 
         public static IElement ShouldHaveType(this IElement element, string expected)
-        {
-            element.Attributes["type"].Value.Should().Be(expected);
-            return element;
-        }
+            => element.ShouldHaveAttribute("type", expected);
 
         public static IElement ShouldHaveTitle(this IElement element, string expected)
-        {
-            element.Attributes["title"].Value.Should().Be(expected);
-            return element;
-        }
+            => element.ShouldHaveAttribute("title", expected);
 
         public static IElement ShouldHaveDataTestId(this IElement element, string expected)
-        {
-            element.Attributes["data-test-id"].Value.Should().Be(expected);
-            return element;
-        }
+            => element.ShouldHaveAttribute("data-test-id", expected);
 
         public static IElement ShouldHaveDataTestClass(this IElement element, string expected)
+            => element.ShouldHaveAttribute("data-test-class", expected);
+        
+        public static IElement ShouldHaveAttribute(this IElement element, string attributeName, string expected)
         {
-            element.Attributes["data-test-class"].Value.Should().Be(expected);
+            var attribute = element.Attributes[attributeName];
+
+            attribute.Should().NotBeNull();
+            attribute!.Value.Should().Be(expected);
+            
             return element;
         }
     }
