4af191a444926d34bc6311e4ea063cfe4938fcba
Steve Peirce <=>
2021-09-04 09:24:31 +0100

    Added basic RenderedFragmentAssertions. No custom assertions yet. #4

---
+++ FluentAssertions.BUnit/AssertionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Bunit;
+using FluentAssertions.Primitives;
+
+namespace FluentAssertions.BUnit
+{
+    public static class AssertionExtensions
+    {
+        public static RenderedFragmentAssertions Should(this IRenderedFragment actualValue)
+        {
+            return new RenderedFragmentAssertions(actualValue);
+        }
+    }
+}
